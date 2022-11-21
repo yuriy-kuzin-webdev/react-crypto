ad1a57009f9528e1d43feb1d3db2641ca5ada6d2
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-21 18:47:26 +0200

    added styles to navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 import Coins from "./components/Coins";
 import Navbar from "./components/Navbar";
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {FaCoins} from 'react-icons/fa';
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div>
         <div className="navbar">
             <FaCoins className='icon'/>
-            <h1>Coin <span className='purple'>Search</span></h1>
+            <h1> Coin <span className='purple'>Search</span></h1>
         </div>
     </div>
   )
